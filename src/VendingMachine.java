5ac9f3337c5973a70736660916a951758e6bcd68
MorganV <vandenberg.morgan@gmail.com>
2019-10-30 15:24:18 -0500

    Adding documentation and refactoring input handling

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -12,11 +12,14 @@ public class VendingMachine {
     }
 
     private void setupComms() {
+        // Configure and open serial port for communicating with the Arduino which controls vending spindles
         serialPort = SerialPort.getCommPort(COMPORT);
         serialPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
         serialPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
         serialPort.openPort();
+
         try {
+            // Arduino devices reset after you connect to them. Code sleeps here to give the device time to do that
             System.out.println("Sleeping to wait for Arduino to reset...");
             Thread.sleep(20000);
             System.out.println("Sleeping complete!");
@@ -31,8 +34,8 @@ public class VendingMachine {
 
 
     public boolean vendItem(int itemID, long userID) {
-        Item toVend = database.getItem(itemID);
-        User vendTo = database.getUser(userID);
+        Item toVend = getItem(itemID);
+        User vendTo = getUser(userID);
         if (!database.itemStocked(toVend.slot)) {
             System.out.println("No stock was found for item " + toVend.name);
             return false;
@@ -40,20 +43,21 @@ public class VendingMachine {
         if (database.getCredit(vendTo) < toVend.cost) {
             System.out.println("Not enough $$ available for user " + vendTo.name);
         }
-
         String bufferedID = "" + toVend.slot;
         if (bufferedID.length() < 2)
             bufferedID = "0" + bufferedID;
+        // Format of output to Arduino is *#*#, where ## is the two-digit representation of the item ID to vend
         String toWrite = "*" + bufferedID.charAt(0) + "*" + bufferedID.charAt(1) + "*";
         System.out.println("Vending item " + toVend.name);
-        serialPort.writeBytes(toWrite.getBytes(), toWrite.length());//vend item
+
+        serialPort.writeBytes(toWrite.getBytes(), toWrite.length());// vend item
         database.decrementStock(toVend.slot);
         database.subtractFunds(vendTo, toVend.cost);
         return true;
     }
 
-    public String getName(long id) {
-        return database.getUser(id).name;
+    public User getUser(long id) {
+        return database.getUser(id);
     }
 
     public int getCredit(long id) {
--- src/VendingMachineGUI.java
+++ src/VendingMachineGUI.java
@@ -6,6 +6,7 @@ import org.w3c.dom.Document;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.HashMap;
 
@@ -17,25 +18,44 @@ public class VendingMachineGUI {
     private Scene toLoad;
 
     public VendingMachineGUI() {
+        // Initialize page dictionary
         pages = new HashMap<>();
+        // Get system screen size
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        // Create JFrame which is the container for all GUI elements
         frame = new JFrame();
-        frame.setUndecorated(true);
-        jfxPanel = new JFXPanel();
-        frame.add(jfxPanel);
-        jfxPanel.setSize(screenSize);
-        frame.setSize(screenSize);
-        frame.setVisible(true);
-        jfxPanel.setVisible(true);
-        frame.setAutoRequestFocus(true);
+        // Make GUI frame fullscreen
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice dev = env.getDefaultScreenDevice();
         dev.setFullScreenWindow(frame);
+        frame.setUndecorated(true);
+        // Add wrapper panel for JavaFX scenes (each page of the GUI)
+        jfxPanel = new JFXPanel();
+        frame.add(jfxPanel);
+
+        // Transparent 16 x 16 pixel cursor image.
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        // Create a new blank cursor.
+        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+                cursorImg, new Point(0, 0), "blank cursor");
+        // Set the blank cursor to the JFrame.
+        frame.getContentPane().setCursor(blankCursor);
+        // Set size of panel / frame to screen dimension
+        jfxPanel.setSize(screenSize);
+        frame.setSize(screenSize);
+        // Make elements visible
+        frame.setVisible(true);
+        jfxPanel.setVisible(true);
+        // Set screen focus
+        frame.setAutoRequestFocus(true);
+
     }
 
     public void addPage(int ID, String pageHTML) {
+        // Load local HTML as resource to render
         URL url = getClass().getResource(pageHTML);
         Platform.runLater(() -> {
+            // Load HTML to webView resource, add to page dictionary
             WebView view = new WebView();
             view.getEngine().load(url.toExternalForm());
             pages.put(ID, view);
@@ -43,9 +63,15 @@ public class VendingMachineGUI {
     }
 
     public void loadPage(int pageName) {
-        Platform.runLater(() -> { // FX components need to be managed by JavaFX
-            if(toLoad==null)toLoad = new Scene(pages.get(pageName));
-            else toLoad.setRoot(pages.get(pageName));
+        Platform.runLater(() -> {
+            // FX components need to be managed by JavaFX thread
+            if (toLoad == null)
+                toLoad = new Scene(pages.get(pageName));
+            else
+                // setRoot is required for JavaFX pages to load correctly
+                toLoad.setRoot(pages.get(pageName));
+
+            // Add event listener for key typing
             toLoad.setOnKeyTyped(event ->
                     MainDriver.keyTyped(event)
             );
@@ -53,13 +79,13 @@ public class VendingMachineGUI {
         });
     }
 
-    public void changePageElement(int pageID, String elementID, String text) {
+    public void modifyPageHTML(int pageID, String elementID, String text) {
         Platform.runLater(() -> {
+            // Get resource for page and set element as specified
             WebView webView = pages.get(pageID);
             Document d = webView.getEngine().getDocument();
             d.getElementById(elementID).setTextContent(text);
             pages.put(pageID, webView);
-
         });
     }
 }
